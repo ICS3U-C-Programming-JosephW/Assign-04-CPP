e33abd30833da0edc1dc8fc4d7c31a4e695f6bae
JosephWondimagnehu <joseph.wondimagnehu@stu.ocsb.ca.>
2025-05-10 13:52:39 -0400

    Added and finished void effect function in aNullEncounter.cpp.

--- aNullEncounter.cpp
+++ aNullEncounter.cpp
@@ -19,6 +19,9 @@ the delay function. */
 /* Include the iostream library for
 input and output functionalities. */
 #include <iostream>
+/* Include the cmath library for
+the power function. */
+#include <cmath>
 /* Include the random library for
 random generators. */
 #include <random>
@@ -157,11 +160,11 @@ float AskQuestions(const std::vector<std::vector
     std::random_device randomDevice;
     /* Use the mersenne twister
     for high-quality randomizing. */ 
-    std::mt19937 mersenneTwister(randomDevice());
+    std::mt19937 randomGen(randomDevice());
 
     // Shuffle the array copy for randomized questions.
     std::shuffle(stageEntriesCopy.begin(),
-    stageEntriesCopy.end(), mersenneTwister);
+    stageEntriesCopy.end(), randomGen);
 
     // Display the initial line for any stage.
     std::cout << LIGHT_BLUE << "Let us see..."
@@ -202,12 +205,98 @@ float AskQuestions(const std::vector<std::vector
             break;
         }
     }
-    
+
     // Return the amount of lives left.
     return lives;
 }
 
-// Runs the main function.
+/* Define a function to generate a possible
+void effect which challenges the user. */
+float chanceVoidEFfect(int stageNum, float lives) {
+    /* Declare the constant void
+    effects array as a vector. */
+    const std::vector<std::string> VOID_EFFECTS = {
+        "The distant stars glare at the unfamiliar "
+        "figure that is you, setting you ablaze internally",
+        "You feel a deep pang of loneliness that eats you inside, "
+        "even though null is by your side",
+        "The temperature of this silent and empty void "
+        "sends powerful chills down your spine",
+        "The lack of entropy present in this void scares you",
+        "Some meteoroids seem to have their own mind and fly "
+        "towards you without your notice, surprising both "
+        "you and null",
+        "Lingering in this void makes you feel unwell, "
+        "and you somewhat feel like leaving",
+    };
+    // Set a constant for purple text.
+    const std::string LIGHT_PURPLE = "\033[1;35m";
+
+    /* Determine the chance of the effect taking
+    place based on the user's stage number. */
+    int effectChance = (0.5 * pow(stageNum, 2))
+    + (0.5 * stageNum) + 39;
+
+    // Check if lives are less than or equal to 0.
+    if (lives <= 0) {
+        /* Return the lives early in the function so
+        the user does not take any extra damage. */
+        return lives;
+    }
+
+    // Set another random device to construct the engine.
+    std::random_device randomChanceDevice;
+    // Use the mersenne twister for high-quality randomizing. 
+    std::mt19937 randomIntGen(randomChanceDevice());
+    /* Uniformly distribute the possible integers
+    over a distance of 1 to 100.*/
+    std::uniform_int_distribution<int> intDist(1, 100);
+
+    /* Check if the generated integer from 1 to 100
+    is less than or equal to the effect chance. */
+    if (intDist(randomIntGen) <= effectChance) {
+        /* Uniformly distribute the possible integers
+        over a distance of 0 to the array size
+        subtracted by one. */
+        std::uniform_int_distribution<int>
+        effectDist(0, VOID_EFFECTS.size() - 1);
+
+        /* Determine the possible effect by randomly
+        selecting one from the void effects array. */
+        std::string possibleEffect = VOID_EFFECTS
+        [effectDist(randomIntGen)];
+
+        /* Determine the damage of the effect
+        based on the user's stage number. */
+        float effectDamage = (0.1 * pow(stageNum, 2))
+        + (0.4 * stageNum) + 0.6;
+
+        /* Take away the user's lives
+        based on the effect damage. */
+        lives = std::max(lives - effectDamage, 0.0f);
+
+        /* Display the resulting effect
+        description and damage dealt. */
+        std::cout << "\n" << LIGHT_PURPLE << possibleEffect << ". "
+        << "You took " << effectDamage << "damage " << "and have " 
+        << lives << ((lives == 1) ? "life": "lives") << "left." 
+        << WHITE << "\n";   
+    } else {
+        /* Otherwise, the random number was greater than
+        the effect chance. */
+        // Display to the user that they are unaffected.
+        std::cout << "\n" << LIGHT_PURPLE << "You remain unaffected "
+        << "in the seemingly soundless void." << WHITE << "\n";
+    }
+    /* Return the lives at the end if the function
+    was able to avoid the early return statement. */
+    return lives;
+}
+
+// Run the main function.
 int main() {
+    /* Create a 3-dimensional vector array to store all the
+    corresponding questions and answers for each of the six
+    stages. Includes types to avoid type complexity. */
 
 }
